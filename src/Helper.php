701dbc84ed6f7913f6d322be6b9dc58ffdaefb08
anounymous <anounymous@none.com>
2024-02-05 17:51:07 +0330

    feat: (branch: feature/get-root-directory-helper) => add Helper::root() method that finds and returns project root directory.

--- src/Helper.php
+++ src/Helper.php
@@ -18,4 +18,31 @@ class Helper
 
         exit;
     }
+
+    /**
+     * Returns app root directory
+     *
+     * It finds parent directory that
+     * contains composer.json file
+     *
+     * @return string|null
+     */
+    public static function root():string|null
+    {
+        $cwd = __DIR__;
+
+        $counter = 0;
+
+        do{
+            if(($cwd = dirname($cwd)) == '.')
+                return null;
+
+            if(in_array(
+                $cwd . DIRECTORY_SEPARATOR . 'composer.json',
+                glob($cwd . DIRECTORY_SEPARATOR . '*json')
+            ))
+                return $cwd;
+
+        } while(1);
+    }
 }
